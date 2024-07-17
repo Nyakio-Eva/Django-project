69f49c44e76d860570bf3b37cc2c963e3115e84b
Nyakio-Eva <evalkariuki@gmail.com>
2024-07-17 22:07:17 +0300

    create template to display members and their details

--- myworld/my_tennis_club/members/views.py
+++ myworld/my_tennis_club/members/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
-
-
-def members(request):
+def home(request):
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = get_object_or_404(Member, id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
