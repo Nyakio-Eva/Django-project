f8bd931c32b0b7cc622cec1ddcaf4ed666c14119
Nyakio-Eva <evalkariuki@gmail.com>
2024-07-19 10:45:02 +0300

    add master, test and main templates

--- myworld/my_tennis_club/members/views.py
+++ myworld/my_tennis_club/members/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from .models import Member
 
 def home(request):
-    template = loader.get_template('myfirst.html')
+    template = loader.get_template('main.html')
     return HttpResponse(template.render())
 
 def members(request):
@@ -20,5 +20,12 @@ def details(request, id):
     template = loader.get_template('details.html')
     context = {
         'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def testing(request):
+    template = loader.get_template('testing.html')
+    context = {
+        'fruits': ['Apple', 'Banana', 'Cherry'],
     }
     return HttpResponse(template.render(context, request))
--- myworld/my_tennis_club/my_tennis_club/settings.py
+++ myworld/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_48sx7wsomnm@(6a40ncwz!1k82tbtb=3hu$xmkr7%-&ku#=0o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
